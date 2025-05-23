038c7ee71e6d9f2b2e303212eab7612c51bf7b77
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-23 12:50:55 +0200

    Refactor flowchartResult directive to handle new data formats; enhance node and connection merging logic for improved visualization

--- flowchart-result/flowchart-result.component.js
+++ flowchart-result/flowchart-result.component.js
@@ -13,7 +13,7 @@ angular.module('app')
                 $scope.$watch('result', function (newVal) {
                     // Log for debugging
                     console.log('[flowchart-result] input result:', newVal);
-                    if (!newVal || !Array.isArray(newVal) || newVal.length === 0) {
+                    if (!newVal || (Array.isArray(newVal) && newVal.length === 0) || (typeof newVal === 'object' && Object.keys(newVal).length === 0)) {
                         $scope.staticChart = null;
                         return;
                     }
@@ -21,50 +21,89 @@ angular.module('app')
                     var chartViewModel = $scope.$parent.chartViewModel;
                     var chartNodes = (chartViewModel && chartViewModel.data && chartViewModel.data.nodes) || [];
                     var chartConnections = (chartViewModel && chartViewModel.data && chartViewModel.data.connections) || [];
-                    // Map abstractservice_id or id to result item
-                    var idToResult = {};
-                    newVal.forEach(function (item) {
-                        var id = item.abstractservice_id || item.id || item.name;
-                        if (id !== undefined) idToResult[id] = item;
-                    });
-                    // Merge chart node data with advised result
-                    var mergedNodes = chartNodes.filter(function (n) {
-                        return idToResult[n.id] !== undefined || idToResult[n.name] !== undefined;
-                    }).map(function (n) {
-                        var id = n.id;
-                        var resultItem = idToResult[id] || idToResult[n.name];
-                        // Merge all properties, prefer resultItem for label/name
-                        return Object.assign({}, n, resultItem || {}, {
-                            name: (resultItem && (resultItem.abstractservice_name || resultItem.name)) || n.name,
-                            id: n.id,
-                            x: n.x,
-                            y: n.y,
-                            width: n.width,
-                            height: n.height,
-                            type: n.type,
-                            image: n.image,
-                            quantity: n.quantity
-                        });
-                    });
-                    // Only include connections where both nodes are present in mergedNodes
-                    var presentIds = new Set(mergedNodes.map(n => n.id));
-                    // Merge connections: if the result contains connection info, merge it, otherwise use chartConnections
-                    var resultConnections = (newVal.connections && Array.isArray(newVal.connections)) ? newVal.connections : [];
+
+                    var mergedNodes = [];
                     var mergedConnections = [];
-                    if (resultConnections.length > 0) {
-                        // Merge result connections, but only if both nodes are present
-                        mergedConnections = resultConnections.filter(function (conn) {
-                            return presentIds.has(conn.from) && presentIds.has(conn.to);
-                        }).map(function (conn) {
-                            return Object.assign({}, conn);
+                    var presentIds = new Set();
+
+                    if (Array.isArray(newVal)) {
+                        // Old format: array of nodes (possibly with .connections)
+                        var idToResult = {};
+                        newVal.forEach(function (item) {
+                            var id = item.abstractservice_id || item.id || item.name;
+                            if (id !== undefined) idToResult[id] = item;
                         });
-                    } else {
-                        // Fallback: use chartConnections
-                        mergedConnections = chartConnections.filter(function (conn) {
-                            return presentIds.has(conn.from) && presentIds.has(conn.to);
-                        }).map(function (conn) {
-                            return Object.assign({}, conn);
+                        mergedNodes = chartNodes.filter(function (n) {
+                            return idToResult[n.id] !== undefined || idToResult[n.name] !== undefined;
+                        }).map(function (n) {
+                            var id = n.id;
+                            var resultItem = idToResult[id] || idToResult[n.name];
+                            return Object.assign({}, n, resultItem || {}, {
+                                name: (resultItem && (resultItem.abstractservice_name || resultItem.name)) || n.name,
+                                id: n.id,
+                                x: n.x,
+                                y: n.y,
+                                width: n.width,
+                                height: n.height,
+                                type: n.type,
+                                image: n.image,
+                                quantity: n.quantity
+                            });
                         });
+                        presentIds = new Set(mergedNodes.map(n => n.id));
+                        var resultConnections = (newVal.connections && Array.isArray(newVal.connections)) ? newVal.connections : [];
+                        if (Array.isArray(resultConnections) && resultConnections.length > 0) {
+                            mergedConnections = resultConnections.filter(function (conn) {
+                                return presentIds.has(conn.from) && presentIds.has(conn.to);
+                            }).map(function (conn) {
+                                return Object.assign({}, conn);
+                            });
+                        } else {
+                            mergedConnections = chartConnections.filter(function (conn) {
+                                return presentIds.has(conn.from) && presentIds.has(conn.to);
+                            }).map(function (conn) {
+                                return Object.assign({}, conn);
+                            });
+                        }
+                    } else if (typeof newVal === 'object' && Array.isArray(newVal.nodes)) {
+                        // New format: object with nodes and connections
+                        var idToResult = {};
+                        newVal.nodes.forEach(function (item) {
+                            var id = item.abstractservice_id || item.id || item.name;
+                            if (id !== undefined) idToResult[id] = item;
+                        });
+                        mergedNodes = chartNodes.filter(function (n) {
+                            return idToResult[n.id] !== undefined || idToResult[n.name] !== undefined;
+                        }).map(function (n) {
+                            var id = n.id;
+                            var resultItem = idToResult[id] || idToResult[n.name];
+                            return Object.assign({}, n, resultItem || {}, {
+                                name: (resultItem && (resultItem.abstractservice_name || resultItem.name || resultItem.label)) || n.name,
+                                id: n.id,
+                                x: n.x,
+                                y: n.y,
+                                width: n.width,
+                                height: n.height,
+                                type: n.type,
+                                image: n.image,
+                                quantity: n.quantity
+                            });
+                        });
+                        presentIds = new Set(mergedNodes.map(n => n.id));
+                        var resultConnections = (Array.isArray(newVal.connections)) ? newVal.connections : [];
+                        if (Array.isArray(resultConnections) && resultConnections.length > 0) {
+                            mergedConnections = resultConnections.filter(function (conn) {
+                                return presentIds.has(conn.from) && presentIds.has(conn.to);
+                            }).map(function (conn) {
+                                return Object.assign({}, conn);
+                            });
+                        } else {
+                            mergedConnections = chartConnections.filter(function (conn) {
+                                return presentIds.has(conn.from) && presentIds.has(conn.to);
+                            }).map(function (conn) {
+                                return Object.assign({}, conn);
+                            });
+                        }
                     }
                     // Log for debugging
                     console.log('[flowchart-result] mergedNodes:', mergedNodes);
