4ea8ca77e370b28039d24cd1b44ccdb7a997f623
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-06-05 10:33:58 +0200

    Update default deployment icon path and add missing PNG extension

--- result/result.component.js
+++ result/result.component.js
@@ -216,8 +216,9 @@ angular.module('app')
                                 node.data.icon = $scope.gcpDeploymentIcons[serviceName];
                             }
                         } else {
-                            node.data.icon = 'assets/deploy-icons/generic-cloud-icon'; // Default icon for nodes without a service
+                            node.data.icon = 'assets/deploy-icons/generic-cloud-icon.png'; // Default icon for nodes without a service
                         }
+                        console.log(node.data.icon);
                     });
                 }, true);
 
